80e4ec33fc0acaefe4aa9bb18c71962242d88d94
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-28 10:55:35 +0300

    Update lab15.5.cpp

--- lab15.5.cpp
+++ lab15.5.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
   int n;
   cout<<"Vvedite N"<<endl;
   cin>>n;
-  int a[n];
+  vector<int> a(n);
   cout<<"Vvedite massiv"<<endl;
   for (int i=0; i<n; i++)
   {
